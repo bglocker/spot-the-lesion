ca5eb90229b419df4280be4d71d660efc604310c
Calin <ca4618@ic.ac.uk>
2020-10-13 19:38:38 +0300

    feat: Further change the button design and add logo to home page [with amr818]

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Container } from "@material-ui/core";
+import logo from "./doctor2.png";
 
 const useStyles = makeStyles({
   root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
+    background: "linear-gradient(315deg, #045de9 0%, #09c6f9 74%)",
+    border: 1,
+    borderRadius: 50,
     boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
     color: "white",
-    height: 50,
+    height: 80,
     padding: "0 30px",
-    width: 100,
+    width: 650,
+    fontSize: 36,
+    fontFamily: "roboto",
   },
   container: {
     height: "100vh",
@@ -26,20 +29,27 @@ const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
   const styles = useStyles();
   return (
     <Container className={styles.container}>
-      <p>Home</p>
+      <p>
+        <div>
+          <img src={logo} alt="this is a logo" />
+        </div>
+      </p>
       <Button className={styles.root} onClick={() => setRoute("game")}>
         Play
       </Button>
+      <br />
       <div>
         <Button className={styles.root} onClick={() => setRoute("tutorial1")}>
           Tutorial
         </Button>
       </div>
+      <br />
       <div>
         <Button className={styles.root} onClick={() => setRoute("about")}>
           About
         </Button>
       </div>
+      <br />
       <div>
         <Button className={styles.root} onClick={() => setRoute("credits")}>
           Credits
