7b1494c701b4e07d2df19c698ef5c87dba6754ef
Calin <ca4618@ic.ac.uk>
2020-10-13 21:29:00 +0300

    feat: Add side images to home page and rotate them [with amr818]

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Container } from "@material-ui/core";
 import logo from "./logo.png";
+import scan from "./ct-scan.png";
+import brain from "./brain.png";
 
 const useStyles = makeStyles({
   root: {
@@ -21,6 +23,28 @@ const useStyles = makeStyles({
     justifyContent: "center",
     alignItems: "center",
   },
+  containerLeftImage: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "left",
+    position: "absolute",
+    left: "200px",
+    top: "100px",
+    transform: "rotate(330deg)",
+  },
+  containerRightImage: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: "10px",
+    top: "285px",
+    transform: "rotate(45deg)",
+  },
 });
 
 const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
@@ -32,35 +56,47 @@ const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
       }}
     >
       <Container className={styles.container}>
-        <p>
+        <Container className={styles.containerLeftImage}>
           <div>
-            <img width="700" height="750" src={logo} alt="this is a logo" />
+            <img width="500" height="500" src={scan} alt="this is a logo" />
           </div>
-        </p>
-        <br />
-        <br />
-        <br />
-        <Button className={styles.root} onClick={() => setRoute("game")}>
-          Play
-        </Button>
-        <br />
-        <div>
-          <Button className={styles.root} onClick={() => setRoute("tutorial1")}>
-            How to Play
+        </Container>
+        <Container className={styles.container}>
+          <p>
+            <div>
+              <img width="700" height="750" src={logo} alt="this is a logo" />
+            </div>
+          </p>
+          <br />
+          <br />
+          <br />
+          <Button className={styles.root} onClick={() => setRoute("game")}>
+            Play
           </Button>
-        </div>
-        <br />
-        <div>
-          <Button className={styles.root} onClick={() => setRoute("about")}>
-            About CT Scans
-          </Button>
-        </div>
-        <br />
-        <div>
-          <Button className={styles.root} onClick={() => setRoute("credits")}>
-            Credits
-          </Button>
-        </div>
+          <br />
+          <div>
+            <Button className={styles.root} onClick={() => setRoute("tutorial1")}>
+              How to Play
+            </Button>
+          </div>
+          <br />
+          <div>
+            <Button className={styles.root} onClick={() => setRoute("about")}>
+              About CT Scans
+            </Button>
+          </div>
+          <br />
+          <div>
+            <Button className={styles.root} onClick={() => setRoute("credits")}>
+              Credits
+            </Button>
+          </div>
+        </Container>
+        <Container className={styles.containerRightImage}>
+          <div>
+            <img width="500" height="500" src={brain} alt="this is a logo" />
+          </div>
+        </Container>
       </Container>
     </div>
   );
