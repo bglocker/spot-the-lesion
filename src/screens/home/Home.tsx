98d6c2097d1b6fc899fbae81e6537316ba4e6213
Calin <ca4618@ic.ac.uk>
2020-10-13 20:53:33 +0300

    feat: Add background colour and change image to be transparent over background and improve general style of fonts [with amr818]

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Container } from "@material-ui/core";
-import logo from "./doctor2.png";
+import logo from "./logo.png";
 
 const useStyles = makeStyles({
   root: {
-    background: "linear-gradient(315deg, #045de9 0%, #09c6f9 74%)",
-    border: 1,
+    background: "#0063B2FF",
     borderRadius: 50,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
     color: "white",
-    height: 80,
-    padding: "0 30px",
-    width: 650,
+    height: 100,
+    width: 1000,
     fontSize: 36,
-    fontFamily: "roboto",
+    fontFamily: "segoe UI",
+    fontWeight: "bold",
   },
   container: {
     height: "100vh",
@@ -28,34 +26,43 @@ const useStyles = makeStyles({
 const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
   const styles = useStyles();
   return (
-    <Container className={styles.container}>
-      <p>
+    <div
+      style={{
+        backgroundColor: "#9CC3D5FF",
+      }}
+    >
+      <Container className={styles.container}>
+        <p>
+          <div>
+            <img width="700" height="750" src={logo} alt="this is a logo" />
+          </div>
+        </p>
+        <br />
+        <br />
+        <br />
+        <Button className={styles.root} onClick={() => setRoute("game")}>
+          Play
+        </Button>
+        <br />
         <div>
-          <img src={logo} alt="this is a logo" />
+          <Button className={styles.root} onClick={() => setRoute("tutorial1")}>
+            How to Play
+          </Button>
         </div>
-      </p>
-      <Button className={styles.root} onClick={() => setRoute("game")}>
-        Play
-      </Button>
-      <br />
-      <div>
-        <Button className={styles.root} onClick={() => setRoute("tutorial1")}>
-          Tutorial
-        </Button>
-      </div>
-      <br />
-      <div>
-        <Button className={styles.root} onClick={() => setRoute("about")}>
-          About
-        </Button>
-      </div>
-      <br />
-      <div>
-        <Button className={styles.root} onClick={() => setRoute("credits")}>
-          Credits
-        </Button>
-      </div>
-    </Container>
+        <br />
+        <div>
+          <Button className={styles.root} onClick={() => setRoute("about")}>
+            About CT Scans
+          </Button>
+        </div>
+        <br />
+        <div>
+          <Button className={styles.root} onClick={() => setRoute("credits")}>
+            Credits
+          </Button>
+        </div>
+      </Container>
+    </div>
   );
 };
 
