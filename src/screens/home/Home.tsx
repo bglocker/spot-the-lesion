ac55afd057b479532a09ee8aee84e33feb832182
tibigg <tg4018@ic.ac.uk>
2020-10-14 17:23:22 +0300

    feat: Turn side images invisible on mobile & window resize [with ca4618, amr818]

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, ButtonGroup, Grid } from "@material-ui/core";
-import logo from "./logo.png";
+import logo from "./doctor.png";
 import scan from "./ct-scan.png";
 import brain from "./brain.png";
 
-const useStyles = makeStyles((style) => ({
+const useStyles = makeStyles({
   logoStyle: {
     alignItems: "center",
     marginTop: "5vh",
@@ -14,7 +14,9 @@ const useStyles = makeStyles((style) => ({
   },
   root: {
     background: "#07575B",
-    borderRadius: 50,
+    borderRadius: 25,
+    borderColor: "black",
+    borderWidth: 10,
     color: "white",
     height: 100,
     width: 1000,
@@ -22,7 +24,6 @@ const useStyles = makeStyles((style) => ({
     fontFamily: "segoe UI",
     fontWeight: "bold",
     marginTop: "2vh",
-    margin: style.spacing(1),
   },
   container: {
     height: "100vh",
@@ -31,10 +32,27 @@ const useStyles = makeStyles((style) => ({
     justifyContent: "center",
     alignItems: "center",
   },
-}));
+  invisible: {
+    display: "none",
+  },
+});
 
 const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
   const styles = useStyles();
+  const windowMinWidth = 2017;
+  const [windowFitsWidth, setIsWindowWideEnough] = useState(window.innerWidth > windowMinWidth);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        const newWindowFitsWidth: boolean = window.innerWidth > windowMinWidth;
+        if (newWindowFitsWidth !== windowFitsWidth) setIsWindowWideEnough(newWindowFitsWidth);
+      },
+      false
+    );
+  }, [windowFitsWidth]);
+
   return (
     <div
       style={{
@@ -45,7 +63,7 @@ const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
         <img width="700" height="750" src={logo} alt="this is a logo" />
       </div>
       <Grid container direction="row" justify="space-evenly" alignItems="center">
-        <div>
+        <div className={!windowFitsWidth ? styles.invisible : ""}>
           <img width="500" height="500" src={scan} alt="this is a logo" />
         </div>
         <ButtonGroup orientation="vertical">
@@ -62,7 +80,7 @@ const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
             Credits
           </Button>
         </ButtonGroup>
-        <div>
+        <div className={!windowFitsWidth ? styles.invisible : ""}>
           <img width="500" height="500" src={brain} alt="this is a logo" />
         </div>
       </Grid>
