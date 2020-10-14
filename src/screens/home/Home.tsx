d4e7075cacb40c436ce3368b037adc9c21fe00d3
calin <calin.biberea18@imperial.ac.uk>
2020-10-14 22:59:08 +0100

    feat: Modify home page to have more responsive buttons

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,58 +1,109 @@
 import React, { useEffect, useState } from "react";
-import { Button, ButtonGroup, Grid } from "@material-ui/core";
-import logo from "./doctor.png";
+import { Button, ButtonGroup, Container, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import logo from "./logo.gif";
 import scan from "./ct-scan.png";
 import brain from "./brain.png";
-import useStyles from "./Styles";
+
+const useStyles = makeStyles({
+  button: {
+    background: "#07575B",
+    borderRadius: 25,
+    borderColor: "black",
+    borderWidth: 10,
+    color: "white",
+    fontFamily: "segoe UI",
+    fontWeight: "bold",
+    marginTop: "2vh",
+  },
+  container: {
+    width: "100vw",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    marginTop: "1vh",
+    width: "auto",
+    height: "500px",
+  },
+  visible: {
+    width: "300px",
+    height: "auto",
+    display: "block",
+  },
+  invisible: {
+    display: "none",
+  },
+});
 
 const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
   const styles = useStyles();
-  const windowMinWidth = 2017;
-  const [windowFitsWidth, setIsWindowWideEnough] = useState(window.innerWidth > windowMinWidth);
+  const minWidth = 2017;
+  const [fullWidth, setFullWidth] = useState(window.innerWidth > minWidth);
 
   useEffect(() => {
     window.addEventListener(
       "resize",
       () => {
-        const newWindowFitsWidth: boolean = window.innerWidth > windowMinWidth;
-        if (newWindowFitsWidth !== windowFitsWidth) setIsWindowWideEnough(newWindowFitsWidth);
+        const newFullWidth: boolean = window.innerWidth > minWidth;
+        setFullWidth(newFullWidth);
       },
       false
     );
-  }, [windowFitsWidth]);
+  }, [fullWidth]);
 
   return (
-    <div
-      style={{
-        backgroundColor: "#66A5AD",
-      }}
-    >
-      <div className={styles.logoStyle}>
-        <img width="700" height="750" src={logo} alt="this is a logo" />
-      </div>
+    <Container className={styles.container}>
+      <img className={styles.logo} src={logo} alt="Spot the Lesion Logo" />
       <Grid container direction="row" justify="space-evenly" alignItems="center">
-        <div className={windowFitsWidth ? styles.visible : styles.invisible}>
-          <img width="500" height="500" src={scan} alt="this is a logo" />
-        </div>
+        <img
+          className={fullWidth ? styles.visible : styles.invisible}
+          src={scan}
+          alt="Scanner.png"
+        />
         <ButtonGroup orientation="vertical">
-          <Button className={styles.button} onClick={() => setRoute("game")}>
+          <Button
+            variant="contained"
+            size="large"
+            className={styles.button}
+            onClick={() => setRoute("game")}
+          >
             Play
           </Button>
-          <Button className={styles.button} onClick={() => setRoute("tutorial1")}>
+          <Button
+            variant="contained"
+            size="large"
+            className={styles.button}
+            onClick={() => setRoute("tutorial1")}
+          >
             How to Play
           </Button>
-          <Button className={styles.button} onClick={() => setRoute("about")}>
+          <Button
+            variant="contained"
+            size="large"
+            className={styles.button}
+            onClick={() => setRoute("about")}
+          >
             About CT Scans
           </Button>
-          <Button className={styles.button} onClick={() => setRoute("credits")}>
+          <Button
+            variant="contained"
+            size="large"
+            className={styles.button}
+            onClick={() => setRoute("credits")}
+          >
             Credits
           </Button>
         </ButtonGroup>
-        <div className={windowFitsWidth ? styles.visible : styles.invisible}>
-          <img width="500" height="500" src={brain} alt="this is a logo" />
-        </div>
+        <img
+          className={fullWidth ? styles.visible : styles.invisible}
+          src={brain}
+          alt="Brain.png"
+        />
       </Grid>
-    </div>
+    </Container>
   );
 };
 
