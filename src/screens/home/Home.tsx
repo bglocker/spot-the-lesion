623ecbb89ceca1a93eccd0f8d0193a1e42b597fa
tibigg <tg4018@ic.ac.uk>
2020-10-14 17:50:00 +0300

    refactor: Move Styles out of Home in separate file [with ca4618, amr818, aob3318]

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,41 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { Button, ButtonGroup, Grid } from "@material-ui/core";
 import logo from "./doctor.png";
 import scan from "./ct-scan.png";
 import brain from "./brain.png";
-
-const useStyles = makeStyles({
-  logoStyle: {
-    alignItems: "center",
-    marginTop: "5vh",
-    display: "flex",
-    justifyContent: "center",
-  },
-  root: {
-    background: "#07575B",
-    borderRadius: 25,
-    borderColor: "black",
-    borderWidth: 10,
-    color: "white",
-    height: 100,
-    width: 1000,
-    fontSize: 36,
-    fontFamily: "segoe UI",
-    fontWeight: "bold",
-    marginTop: "2vh",
-  },
-  container: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  invisible: {
-    display: "none",
-  },
-});
+import useStyles from "./Styles";
 
 const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
   const styles = useStyles();
@@ -63,24 +31,24 @@ const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
         <img width="700" height="750" src={logo} alt="this is a logo" />
       </div>
       <Grid container direction="row" justify="space-evenly" alignItems="center">
-        <div className={!windowFitsWidth ? styles.invisible : ""}>
+        <div className={windowFitsWidth ? styles.visible : styles.invisible}>
           <img width="500" height="500" src={scan} alt="this is a logo" />
         </div>
-        <ButtonGroup orientation="vertical">
-          <Button className={styles.root} onClick={() => setRoute("game")}>
+        <ButtonGroup className={styles.buttonGroup} orientation="vertical">
+          <Button className={styles.button} onClick={() => setRoute("game")}>
             Play
           </Button>
-          <Button className={styles.root} onClick={() => setRoute("tutorial1")}>
+          <Button className={styles.button} onClick={() => setRoute("tutorial1")}>
             How to Play
           </Button>
-          <Button className={styles.root} onClick={() => setRoute("about")}>
+          <Button className={styles.button} onClick={() => setRoute("about")}>
             About CT Scans
           </Button>
-          <Button className={styles.root} onClick={() => setRoute("credits")}>
+          <Button className={styles.button} onClick={() => setRoute("credits")}>
             Credits
           </Button>
         </ButtonGroup>
-        <div className={!windowFitsWidth ? styles.invisible : ""}>
+        <div className={windowFitsWidth ? styles.visible : styles.invisible}>
           <img width="500" height="500" src={brain} alt="this is a logo" />
         </div>
       </Grid>
