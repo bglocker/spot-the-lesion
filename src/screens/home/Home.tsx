5600231c5f668266d68030d5699dbee92ff5b961
Calin <ca4618@ic.ac.uk>
2020-10-15 11:20:50 +0300

    fix: resize components in home page so they fit and resize correctly on both PC and phone

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,46 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { Button, ButtonGroup, Container, Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 import logo from "./logo.gif";
 import scan from "./ct-scan.png";
 import brain from "./brain.png";
 
-const useStyles = makeStyles({
-  button: {
-    background: "#07575B",
-    borderRadius: 25,
-    borderColor: "black",
-    borderWidth: 10,
-    color: "white",
-    fontFamily: "segoe UI",
-    fontWeight: "bold",
-    marginTop: "2vh",
-  },
-  container: {
-    width: "100vw",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  logo: {
-    marginTop: "1vh",
-    width: "auto",
-    height: "500px",
-  },
-  visible: {
-    width: "300px",
-    height: "auto",
-    display: "block",
-  },
-  invisible: {
-    display: "none",
-  },
-});
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    button: {
+      background: "#07575B",
+      borderRadius: 20,
+      borderColor: "black",
+      borderWidth: 4,
+      color: "white",
+      fontFamily: "segoe UI",
+      fontWeight: "bold",
+      marginTop: "3vh",
+      fontSize: 22,
+      width: 300,
+      margin: theme.spacing(2),
+    },
+    container: {
+      width: "100vw",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    logo: {
+      marginTop: "1vh",
+      width: "auto",
+      height: "360px",
+    },
+    visible: {
+      width: "250px",
+      height: "auto",
+      display: "block",
+    },
+    invisible: {
+      display: "none",
+    },
+  })
+);
 
 const Home: React.FC<HomeProps> = ({ setRoute }: HomeProps) => {
   const styles = useStyles();
-  const minWidth = 2017;
+  const minWidth = 1360;
   const [fullWidth, setFullWidth] = useState(window.innerWidth > minWidth);
 
   useEffect(() => {
