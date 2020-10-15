2555fdea7cfc630d05e25e7609004c21b909d8dc
cb3618 <cb3618@ic.ac.uk>
2020-10-15 14:43:37 +0300

    refactor: Use effect hook for all canvas operations

--- src/screens/game/Game.tsx
+++ src/screens/game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button } from "@material-ui/core";
 
 const Game: React.FC<GameProps> = ({ setRoute }: GameProps) => {
@@ -19,6 +19,27 @@ const Game: React.FC<GameProps> = ({ setRoute }: GameProps) => {
   const [clicked, setClicked] = useState(false);
   const [hinted, setHinted] = useState(false);
 
+  type DrawType = ((canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) => void) | null;
+  const [draw, setDraw] = useState<DrawType>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas == null) {
+      return;
+    }
+
+    const context = canvas.getContext("2d");
+    if (context == null) {
+      return;
+    }
+
+    if (draw == null) {
+      return;
+    }
+
+    draw(canvas, context);
+  }, [draw]);
+
   const bbIntersectionOverUnion = (boxA: number[], boxB: number[]): number => {
     const xA = Math.max(boxA[0], boxB[0]);
     const yA = Math.max(boxA[1], boxB[1]);
@@ -34,55 +55,36 @@ const Game: React.FC<GameProps> = ({ setRoute }: GameProps) => {
   };
 
   const clearTimeouts = () => {
-    timeouts.forEach((element) => {
-      clearTimeout(element);
-    });
+    timeouts.forEach((element) => clearTimeout(element));
     setTimeouts([]);
   };
 
-  const drawTruth = () => {
-    if (canvasRef.current == null) {
-      return;
-    }
+  const drawTruth = () =>
+    setDraw(() => (_: HTMLCanvasElement, context: CanvasRenderingContext2D) => {
+      context.beginPath();
+      context.strokeStyle = "yellow";
+      context.lineWidth = 3;
+      context.rect(truth[0], truth[1], truth[2] - truth[0], truth[3] - truth[1]);
+      context.stroke();
 
-    const context = canvasRef.current.getContext("2d");
-    if (context == null) {
-      return;
-    }
+      if (bbIntersectionOverUnion(truth, predicted) > 0.5) {
+        context.strokeStyle = "green";
+      } else {
+        context.strokeStyle = "red";
+      }
 
-    context.beginPath();
-    context.strokeStyle = "yellow";
-    context.lineWidth = 3;
-    context.rect(truth[0], truth[1], truth[2] - truth[0], truth[3] - truth[1]);
-    context.stroke();
-
-    if (bbIntersectionOverUnion(truth, predicted) > 0.5) {
-      context.strokeStyle = "green";
-    } else {
-      context.strokeStyle = "red";
-    }
-
-    context.lineWidth = 3;
-    context.beginPath();
-    context.rect(
-      predicted[0],
-      predicted[1],
-      predicted[2] - predicted[0],
-      predicted[3] - predicted[1]
-    );
-    context.stroke();
-  };
+      context.lineWidth = 3;
+      context.beginPath();
+      context.rect(
+        predicted[0],
+        predicted[1],
+        predicted[2] - predicted[0],
+        predicted[3] - predicted[1]
+      );
+      context.stroke();
+    });
 
   const drawHint = () => {
-    if (canvasRef.current == null) {
-      return;
-    }
-
-    const context = canvasRef.current.getContext("2d");
-    if (context == null) {
-      return;
-    }
-
     if (hinted) {
       return;
     }
@@ -92,43 +94,31 @@ const Game: React.FC<GameProps> = ({ setRoute }: GameProps) => {
     const x = truth[0] + (truth[2] - truth[0]) / 2 + Math.random() * 100 - 50;
     const y = truth[1] + (truth[3] - truth[1]) / 2 + Math.random() * 100 - 50;
 
-    context.beginPath();
-    context.strokeStyle = "red";
-    context.lineWidth = 2;
-    context.arc(x, y, 100, 0, 2 * Math.PI);
-    context.stroke();
+    setDraw(() => (_: HTMLCanvasElement, context: CanvasRenderingContext2D) => {
+      context.beginPath();
+      context.strokeStyle = "red";
+      context.lineWidth = 2;
+      context.arc(x, y, 100, 0, 2 * Math.PI);
+      context.stroke();
+    });
   };
 
   const stopTimer = () => {
     clearInterval(countdown);
   };
 
-  const getMousePosition = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-    const canvas = canvasRef.current;
-    if (canvas == null) {
-      return { x: 0, y: 0 };
-    }
-
+  const getMousePosition = (playerX: number, playerY: number, canvas: HTMLCanvasElement) => {
     const rect = canvas.getBoundingClientRect(); // abs. size of element
     const scaleX = canvas.width / rect.width; // relationship bitmap vs. element for X
     const scaleY = canvas.height / rect.height; // relationship bitmap vs. element for Y
 
     return {
-      x: (event.clientX - rect.left) * scaleX, // scale mouse coordinates after they have
-      y: (event.clientY - rect.top) * scaleY, // been adjusted to be relative to element
+      x: (playerX - rect.left) * scaleX, // scale mouse coordinates after they have
+      y: (playerY - rect.top) * scaleY, // been adjusted to be relative to element
     };
   };
 
   const onCanvasClick = async (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-    if (canvasRef.current == null) {
-      return;
-    }
-
-    const context = canvasRef.current.getContext("2d");
-    if (context == null) {
-      return;
-    }
-
     if (timeRemaining <= 0 || clicked) {
       return;
     }
@@ -138,26 +128,25 @@ const Game: React.FC<GameProps> = ({ setRoute }: GameProps) => {
     drawTruth();
     stopTimer();
 
-    const position = getMousePosition(event);
+    const [playerX, playerY] = [event.clientX, event.clientY];
 
-    if (
-      truth[0] <= position.x &&
-      position.x <= truth[2] &&
-      truth[1] <= position.y &&
-      position.y <= truth[3]
-    ) {
-      setCorrect((prevState) => prevState + 1);
-      context.strokeStyle = "green";
-    } else {
-      context.strokeStyle = "red";
-    }
+    setDraw(() => (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) => {
+      const { x, y } = getMousePosition(playerX, playerY, canvas);
 
-    context.beginPath();
-    context.moveTo(position.x - 5, position.y - 5);
-    context.lineTo(position.x + 5, position.y + 5);
-    context.moveTo(position.x + 5, position.y - 5);
-    context.lineTo(position.x - 5, position.y + 5);
-    context.stroke();
+      if (truth[0] <= x && y <= truth[2] && truth[1] <= x && y <= truth[3]) {
+        setCorrect((prevState) => prevState + 1);
+        context.strokeStyle = "green";
+      } else {
+        context.strokeStyle = "red";
+      }
+
+      context.beginPath();
+      context.moveTo(x - 5, y - 5);
+      context.lineTo(x + 5, y + 5);
+      context.moveTo(x + 5, y - 5);
+      context.lineTo(x - 5, y + 5);
+      context.stroke();
+    });
 
     clearTimeouts();
   };
@@ -199,20 +188,11 @@ const Game: React.FC<GameProps> = ({ setRoute }: GameProps) => {
     // Build the image
     const img = new Image();
     img.src = `${process.env.PUBLIC_URL}/content/images/${id}.png`;
-    img.onload = () => {
-      const canvas = canvasRef.current;
-      if (canvas == null) {
-        return;
-      }
-
-      const canvasContext = canvas.getContext("2d");
-      if (!canvasContext) {
-        return;
-      }
-
-      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-      canvasContext.drawImage(img, 0, 0, canvas.width, canvas.height);
-    };
+    img.onload = () =>
+      setDraw(() => (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+      });
 
     const hintTimeout = window.setTimeout(drawHint, 5000);
     const truthTimeout = window.setTimeout(drawTruth, 10000);
