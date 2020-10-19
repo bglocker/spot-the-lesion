b688ce3509d7f0bb6003826a3213b87e6b43b672
cb3618 <cb3618@ic.ac.uk>
2020-10-19 20:56:18 +0300

    fix: Add check for firebase environment variable

--- src/firebase/firebaseApp.ts
+++ src/firebase/firebaseApp.ts
@@ -1,15 +1,20 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import "firebase/functions";
-import { firebaseConfig } from "./firebaseConfig";
+import firebaseConfig from "./firebaseConfig";
 
-export let firebaseApp: firebase.app.App;
+/* TODO: test with firebase.auth() and firebase.firestore() later on */
 export let auth: firebase.auth.Auth;
 export let db: firebase.firestore.Firestore;
 
 export const setupFirebase = (): void => {
-  firebaseApp = firebase.initializeApp(firebaseConfig);
+  /* TODO: handle this case */
+  if (process.env.REACT_APP_FIREBASE_API_KEY === undefined) {
+    console.error("Firebase api key not set");
+    return;
+  }
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
   auth = firebaseApp.auth();
   db = firebaseApp.firestore();
 };
