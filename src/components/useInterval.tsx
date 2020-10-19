34b677283a608b92f3936f037075c70a2b890534
cb3618 <cb3618@ic.ac.uk>
2020-10-19 15:05:01 +0300

    chore: Document useInterval.tsx

--- src/components/useInterval.tsx
+++ src/components/useInterval.tsx
@@ -1,18 +1,26 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * Custom hook for intervals
+ *
+ * @param callback Function to be called in interval
+ * @param delay    How often to call the function, or null to pause the interval
+ */
 const useInterval = (callback: () => void, delay: number | null): void => {
   const savedCallback = useRef<() => void>();
 
+  /* Update callback to catch state changes */
   useEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
 
+  /* Trigger or pause the interval */
   useEffect(() => {
-    function tick() {
+    const tick = () => {
       if (savedCallback.current !== undefined) {
         savedCallback.current();
       }
-    }
+    };
 
     if (delay !== null) {
       const id = window.setInterval(tick, delay);
