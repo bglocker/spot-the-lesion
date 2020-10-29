5e85d45b56c85d312f0f34270773c1b8bca1ba83
Calin <calin.alexandru9995@gmail.com>
2020-10-29 19:33:19 +0200

    feat: change the pie charts to use nivo instead of Victory

--- src/screens/statistics/Statistics.tsx
+++ src/screens/statistics/Statistics.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { AppBar, CircularProgress, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { KeyboardBackspace } from "@material-ui/icons";
+import { ResponsivePie } from "@nivo/pie";
 import { db } from "../../firebase/firebaseApp";
 
 const useStyles = makeStyles(() =>
@@ -56,6 +57,7 @@ const Statistics: React.FC<StatisticsProps> = ({ setRoute }: StatisticsProps) =>
       </>
     );
   }
+
   return (
     <>
       <AppBar position="sticky">
@@ -73,6 +75,294 @@ const Statistics: React.FC<StatisticsProps> = ({ setRoute }: StatisticsProps) =>
           <Typography>Spot the Lesion</Typography>
         </Toolbar>
       </AppBar>
+      <ResponsivePie
+        data={[
+          {
+            id: "javascript",
+            label: "javascript",
+            value: 123,
+            color: "hsl(332, 70%, 50%)",
+          },
+          {
+            id: "haskell",
+            label: "haskell",
+            value: 123,
+            color: "hsl(194, 70%, 50%)",
+          },
+          {
+            id: "php",
+            label: "php",
+            value: 123,
+            color: "hsl(184, 70%, 50%)",
+          },
+          {
+            id: "sass",
+            label: "sass",
+            value: 123,
+            color: "hsl(325, 70%, 50%)",
+          },
+          {
+            id: "stylus",
+            label: "stylus",
+            value: 197,
+            color: "hsl(14, 70%, 50%)",
+          },
+        ]}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        colors={{ scheme: "red_blue" }}
+        borderWidth={5}
+        borderColor={{ theme: "background" }}
+        radialLabelsSkipAngle={5}
+        radialLabelsTextXOffset={12}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={23}
+        radialLabelsLinkHorizontalLength={32}
+        radialLabelsLinkStrokeWidth={3}
+        radialLabelsLinkColor={{ from: "color" }}
+        enableSlicesLabels={false}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#333333"
+        motionStiffness={90}
+        motionDamping={15}
+        defs={[
+          {
+            id: "dots",
+            type: "patternDots",
+            background: "inherit",
+            color: "rgba(255, 255, 255, 0.3)",
+            size: 4,
+            padding: 1,
+            stagger: true,
+          },
+          {
+            id: "lines",
+            type: "patternLines",
+            background: "inherit",
+            color: "rgba(255, 255, 255, 0.3)",
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10,
+          },
+        ]}
+        fill={[
+          {
+            match: {
+              id: "ruby",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "c",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "go",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "python",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "scala",
+            },
+            id: "lines",
+          },
+          {
+            match: {
+              id: "lisp",
+            },
+            id: "lines",
+          },
+          {
+            match: {
+              id: "elixir",
+            },
+            id: "lines",
+          },
+          {
+            match: {
+              id: "javascript",
+            },
+            id: "lines",
+          },
+        ]}
+        legends={[
+          {
+            anchor: "bottom",
+            direction: "row",
+            translateY: 56,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: "#999",
+            symbolSize: 18,
+            symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000",
+                },
+              },
+            ],
+          },
+        ]}
+      />
+      <ResponsivePie
+        data={[
+          {
+            id: "javascript",
+            label: "javascript",
+            value: 123,
+            color: "hsl(332, 70%, 50%)",
+          },
+          {
+            id: "haskell",
+            label: "haskell",
+            value: 123,
+            color: "hsl(194, 70%, 50%)",
+          },
+          {
+            id: "php",
+            label: "php",
+            value: 123,
+            color: "hsl(184, 70%, 50%)",
+          },
+          {
+            id: "sass",
+            label: "sass",
+            value: 123,
+            color: "hsl(325, 70%, 50%)",
+          },
+          {
+            id: "stylus",
+            label: "stylus",
+            value: 197,
+            color: "hsl(14, 70%, 50%)",
+          },
+        ]}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        colors={{ scheme: "red_blue" }}
+        borderWidth={5}
+        borderColor={{ theme: "background" }}
+        radialLabelsSkipAngle={5}
+        radialLabelsTextXOffset={12}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={23}
+        radialLabelsLinkHorizontalLength={32}
+        radialLabelsLinkStrokeWidth={3}
+        radialLabelsLinkColor={{ from: "color" }}
+        enableSlicesLabels={false}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#333333"
+        motionStiffness={90}
+        motionDamping={15}
+        defs={[
+          {
+            id: "dots",
+            type: "patternDots",
+            background: "inherit",
+            color: "rgba(255, 255, 255, 0.3)",
+            size: 4,
+            padding: 1,
+            stagger: true,
+          },
+          {
+            id: "lines",
+            type: "patternLines",
+            background: "inherit",
+            color: "rgba(255, 255, 255, 0.3)",
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10,
+          },
+        ]}
+        fill={[
+          {
+            match: {
+              id: "ruby",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "c",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "go",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "python",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "scala",
+            },
+            id: "lines",
+          },
+          {
+            match: {
+              id: "lisp",
+            },
+            id: "lines",
+          },
+          {
+            match: {
+              id: "elixir",
+            },
+            id: "lines",
+          },
+          {
+            match: {
+              id: "javascript",
+            },
+            id: "lines",
+          },
+        ]}
+        legends={[
+          {
+            anchor: "bottom",
+            direction: "row",
+            translateY: 56,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: "#999",
+            symbolSize: 18,
+            symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000",
+                },
+              },
+            ],
+          },
+        ]}
+      />
     </>
   );
 };
