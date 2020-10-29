cd3f83c0568cf849d4667ab0d56ab5a591297ba5
Calin Biberea <calinbiberea@gmail.com>
2020-10-29 16:50:16 +0000

    feat(Statistics): Clear dependencies and transition to another chart

--- src/screens/statistics/Statistics.tsx
+++ src/screens/statistics/Statistics.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { AppBar, CircularProgress, Grid, IconButton, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, CircularProgress, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { KeyboardBackspace } from "@material-ui/icons";
-import { CanvasJSChart } from "canvasjs-react-charts";
 import { db } from "../../firebase/firebaseApp";
 
 const useStyles = makeStyles(() =>
@@ -24,103 +23,14 @@ const Statistics: React.FC<StatisticsProps> = ({ setRoute }: StatisticsProps) =>
    */
   const [loading, setLoading] = useState(true);
 
-  const [aiScore, setAiScore] = useState(0);
-  const [humanScore, setHumanScore] = useState(0);
-  const [aiWins, setAiWins] = useState(0);
-  const [humanWins, setHumanWins] = useState(0);
-
   /**
    * Retrieves the data from the databse to display into the pie-chart and graph
    */
   const retrieveStatistics = async () => {
     const snapshot = await db.collection("alltime-scores").get();
-    let aiScoreCounter = 0;
-    let humanScoreCounter = 0;
-    let noOfHumanWins = 0;
-    let noOfGames = 0;
-
-    snapshot.forEach((doc) => {
-      aiScoreCounter += doc.data().ai_score;
-      humanScoreCounter += doc.data().score;
-      if (doc.data().correct_player_answers > doc.data().correct_ai_answers) {
-        noOfHumanWins += 1;
-      }
-      noOfGames += 1;
-    });
-    setAiScore(aiScoreCounter / noOfGames);
-    setHumanScore(humanScoreCounter / noOfGames);
-    setHumanWins(noOfHumanWins);
-    setAiWins(noOfGames - noOfHumanWins);
-
+    console.log(snapshot);
     setLoading(false);
   };
-
-  const getOptionsPieChart = (title: string, aiLabel: string, humanLabel: string) => {
-    return {
-      animationEnabled: true,
-      exportEnabled: true,
-      backgroundColor: "#63A2AB",
-      title: {
-        text: title,
-        fontColor: "white",
-      },
-      data: [
-        {
-          type: "pie",
-          indexLabel: "{label}: {y}",
-          indexLabelFontColor: "white",
-          startAngle: -90,
-          dataPoints: [
-            {
-              y: aiWins,
-              label: aiLabel,
-            },
-            {
-              y: humanWins,
-              label: humanLabel,
-            },
-          ],
-        },
-      ],
-    };
-  };
-
-  const getOptionsContainerChart = (title: string, aiLabel: string, humanLabel: string) => {
-    return {
-      animationEnabled: true,
-      exportEnabled: true,
-      backgroundColor: "#63A2AB",
-      title: {
-        text: title,
-        fontColor: "white",
-      },
-      axisX: {
-        labelFontSize: "32",
-        labelFontColor: "white",
-      },
-      data: [
-        {
-          type: "column",
-          indexLabel: "{label}: {y}",
-          indexLabelFontColor: "white",
-          startAngle: -90,
-          dataPoints: [
-            {
-              y: aiScore,
-              label: aiLabel,
-            },
-            {
-              y: humanScore,
-              label: humanLabel,
-            },
-          ],
-        },
-      ],
-    };
-  };
-
-  const options1 = getOptionsPieChart("Human vs AI", "AI Victories", "Human Victories");
-  const options2 = getOptionsContainerChart("Average Scores", "AI Average", "Human Average");
 
   if (loading) {
     retrieveStatistics();
@@ -163,15 +73,6 @@ const Statistics: React.FC<StatisticsProps> = ({ setRoute }: StatisticsProps) =>
           <Typography>Spot the Lesion</Typography>
         </Toolbar>
       </AppBar>
-
-      <Grid>
-        <div>
-          <CanvasJSChart options={options1} />
-        </div>
-        <div>
-          <CanvasJSChart options={options2} />
-        </div>
-      </Grid>
     </>
   );
 };
