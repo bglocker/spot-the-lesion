ac3fb8cbf4ed4169bf81c177be0611802fc88eef
cb3618 <cb3618@ic.ac.uk>
2020-11-13 00:43:09 +0200

    refactor(HeatmapDisplay): Move getClickedPoints inside useEffect

--- src/components/HeatmapDisplay.tsx
+++ src/components/HeatmapDisplay.tsx
@@ -18,81 +18,87 @@ const useStyles = makeStyles(() =>
   })
 );
 
+interface ImageData {
+  clicks: { clickCount: number; x: number; y: number }[];
+  correctClicks: number;
+  hintCount: number;
+  wrongClicks: number;
+}
+
 const HeatmapDisplay: React.FC<HeatmapProps> = ({ imageId, imageUrl }: HeatmapProps) => {
   const classes = useStyles();
 
   const [heatmapContainer, setHeatmapContainer] = useState<HTMLDivElement | null>(null);
+  const [heatmapInstance, setHeatmapInstance] = useState<h337>(null!);
+  const [resize, setResize] = useState(false);
+
   const heatmapContainerRef = useCallback((div) => {
     if (div !== null) {
       setHeatmapContainer(div);
     }
   }, []);
 
-  const getClickedPoints = useCallback(async () => {
-    const docName = `image_${imageId}`;
-    const snapshot = await db.collection(DbUtils.IMAGES).doc(docName).get();
-    const data = snapshot.data();
+  /* Track window resizing */
+  useEffect(() => {
+    const onResize = () => setResize(true);
 
-    if (data === undefined) {
-      return [];
-    }
+    window.addEventListener("resize", onResize);
 
-    const clicks: { x: number; y: number; value: number }[] = [];
-
-    for (let i = 0; i < data.clicks.length; i++) {
-      clicks.push({ x: data.clicks[i].x, y: data.clicks[i].y, value: data.clicks[i].clickCount });
-    }
-
-    return clicks;
-  }, [imageId]);
-
-  const [heatmapInstance, setHeatmapInstance] = useState<h337>(null!);
-  const [resize, setResize] = useState(false);
-
-  const resizeHandler = useCallback(() => {
-    window.removeEventListener("resize", resizeHandler);
-    setResize(true);
+    return () => window.removeEventListener("resize", onResize);
   }, []);
 
   useEffect(() => {
+    const getClicks = async () => {
+      const docName = `image_${imageId}`;
+
+      /* TODO: error handling */
+      const snapshot = await db.collection(DbUtils.IMAGES).doc(docName).get();
+      const data = snapshot.data();
+
+      if (data === undefined) {
+        /* TODO: error handling */
+        return [];
+      }
+
+      return (data as ImageData).clicks.map(({ x, y, clickCount }) => ({
+        x,
+        y,
+        value: clickCount,
+      }));
+    };
+
     if (heatmapContainer === null) {
       return;
     }
 
-    if (resize) {
+    /* Clear previous heatmap before redrawing due to resize */
+    if (heatmapInstance !== null && resize) {
       // eslint-disable-next-line no-underscore-dangle
       const { canvas } = heatmapInstance._renderer;
+
       canvas.remove();
       setHeatmapInstance(null);
-
       setResize(false);
     }
 
+    /* Create heatmap instance and draw clicks */
     if (heatmapInstance === null) {
-      setHeatmapInstance(
-        h337.create({
-          container: heatmapContainer,
-        })
-      );
-      return;
+      const instance = h337.create({ container: heatmapContainer });
+
+      setHeatmapInstance(instance);
+
+      getClicks().then((clicks) => {
+        instance.setData({
+          max: 1,
+          data: clicks.map(({ x, y, value }) => ({
+            x: Math.floor((x * heatmapContainer.offsetWidth) / 100),
+            y: Math.floor((y * heatmapContainer.offsetHeight) / 100),
+            value,
+          })),
+        });
+      });
     }
-
-    getClickedPoints().then((clicks) => {
-      const heatmapData = {
-        max: 1,
-        data: clicks.map((click) => {
-          return {
-            x: Math.floor((click.x * heatmapContainer!.offsetWidth) / 100),
-            y: Math.floor((click.y * heatmapContainer!.offsetHeight) / 100),
-            value: click.value,
-          };
-        }),
-      };
-
-      heatmapInstance.setData(heatmapData);
-      window.addEventListener("resize", resizeHandler);
-    });
-  }, [getClickedPoints, heatmapContainer, heatmapInstance, resize, resizeHandler]);
+  }, [heatmapContainer, heatmapInstance, imageId, resize]);
 
   return (
     <div className={classes.container} ref={heatmapContainerRef}>
