a801846ba0a14e1c2691e0cae4a9f5826607e248
aob3318 <aob3318@ic.ac.uk>
2020-11-19 21:14:39 +0200

    feat: Design admin authentication page

--- src/screens/admin_console/AdminAuth.tsx
+++ src/screens/admin_console/AdminAuth.tsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import { firebaseAuth } from "../../firebase/firebaseApp";
+import colors from "../../res/colors";
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme) =>
   createStyles({
     backButton: {
       marginRight: 8,
     },
     container: {
       height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.secondary,
+    },
+    box: {
+      backgroundColor: "white",
+      width: "60%",
+      height: "60%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 24,
+      boxSizing: "border-box",
+    },
+    text: {
+      [theme.breakpoints.only("xs")]: {
+        fontSize: "1.25rem",
+      },
+      [theme.breakpoints.only("sm")]: {
+        fontSize: "1.5rem",
+      },
+      [theme.breakpoints.up("md")]: {
+        fontSize: "2rem",
+      },
+      textAlign: "center",
+      marginBottom: 24,
+    },
+    submit: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    spacing: {
+      margin: 10,
     },
   })
 );
@@ -17,26 +56,42 @@ const useStyles = makeStyles(() =>
 const AdminAuth: React.FC = () => {
   const classes = useStyles();
 
-  firebaseAuth
-    .signInWithEmailAndPassword("spot-the-lesion@gmail.com", "spot2020")
-    .then(() => {
-      // eslint-disable-next-line no-console
-      console.log("Managed to log in");
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.log(error);
-    });
+  const [password, setPassword] = useState<string>("");
+
+  const submitClick = () => {
+    firebaseAuth
+      .signInWithEmailAndPassword("spot-the-lesion@gmail.com", password)
+      .then(() => {
+        // eslint-disable-next-line no-console
+        console.log("Managed to log in");
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  };
 
   return (
     <>
-      <AppBar position="sticky">
+      <AppBar position="absolute">
         <Toolbar variant="dense">
           <Typography>Spot the Lesion</Typography>
         </Toolbar>
       </AppBar>
 
-      <div className={classes.container} />
+      <div className={classes.container}>
+        <div className={classes.box}>
+          <Typography className={classes.text}> Password </Typography>
+          <div className={classes.submit}>
+            <input
+              className={classes.spacing}
+              type="text"
+              onChange={(changeTextbox) => setPassword(changeTextbox.target.value)}
+            />
+            <input className={classes.spacing} type="submit" value="Submit" onClick={submitClick} />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
