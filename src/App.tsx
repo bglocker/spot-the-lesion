d6fa39e5e00270751d81797ef87faff7dec7fe7e
Calin Biberea <calinbiberea@gmail.com>
2020-11-23 21:47:31 +0200

    feat(Admin-Console): Implement screen for taking files

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Leaderboard from "./screens/leaderboard/Leaderboard";
 import Statistics from "./screens/statistics/Statistics";
 import OptionsManager from "./screens/game/selectMode/OptionsManager";
 import Achievements from "./screens/achievements/Achievements";
-import AdminAuth from "./screens/admin_console/AdminAuth";
+import FileUpload from "./screens/admin_console/fileUpload/FileUpload";
 
 const theme = createMuiTheme({
   palette: {
@@ -80,7 +80,7 @@ const App: React.FC = () => {
           </Route>
 
           <Route path="/admin">
-            <AdminAuth />
+            <FileUpload />;
           </Route>
 
           <Route path="*">
---
+++ src/screens/admin_console/fileUpload/FileUpload.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import colors from "../../../res/colors";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    backButton: {
+      marginRight: 8,
+    },
+    container: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.secondary,
+    },
+    box: {
+      backgroundColor: "white",
+      width: "60%",
+      height: "60%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 24,
+      boxSizing: "border-box",
+    },
+    text: {
+      [theme.breakpoints.only("xs")]: {
+        fontSize: "1.25rem",
+      },
+      [theme.breakpoints.only("sm")]: {
+        fontSize: "1.5rem",
+      },
+      [theme.breakpoints.up("md")]: {
+        fontSize: "2rem",
+      },
+      textAlign: "center",
+      marginBottom: 24,
+    },
+    submit: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    spacing: {
+      margin: 10,
+    },
+  })
+);
+
+const FileUpload: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="absolute">
+        <Toolbar variant="dense">
+          <Typography>Spot the Lesion</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <div className={classes.container}>
+        <div className={classes.box}>
+          <Typography className={classes.text}> Password </Typography>
+          <div className={classes.submit}>
+            <input type="file" multiple />
+            <input className={classes.spacing} type="submit" value="Submit" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FileUpload;
