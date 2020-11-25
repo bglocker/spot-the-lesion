0dc92232c6622f4aac750cba3eb5d4cf5ebf5a69
cb3618 <cb3618@ic.ac.uk>
2020-11-25 21:18:05 +0200

    feat(ChallengeDialog): Add copy to clipboard functionality

--- src/screens/game/ChallengeDialog.tsx
+++ src/screens/game/ChallengeDialog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   Button,
   Dialog,
@@ -6,19 +6,87 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Tooltip,
 } from "@material-ui/core";
+import { AssignmentTurnedInOutlined } from "@material-ui/icons";
 
 const ChallengeDialog: React.FC<ChallengeDialogProps> = ({
   open,
   onClose,
   link,
 }: ChallengeDialogProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const [tooltipTitle, setTooltipTitle] = useState("Copy to clipboard");
+
+  const selectInputText = () => {
+    if (inputRef.current === null) {
+      return;
+    }
+
+    inputRef.current.select();
+
+    /* For mobile devices */
+    inputRef.current.setSelectionRange(0, link.length);
+  };
+
+  const onCopyToClipboardClick = () => {
+    selectInputText();
+
+    document.execCommand("copy");
+
+    setTooltipTitle("Copied!");
+  };
+
+  const onMouseEnterAdornment = () => {
+    setTooltipTitle("Copy to clipboard");
+    setTooltipOpen(true);
+  };
+
+  const onMouseLeaveAdornment = () => {
+    setTooltipOpen(false);
+  };
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Challenge</DialogTitle>
 
       <DialogContent>
-        <DialogContentText>{link}</DialogContentText>
+        <DialogContentText>Share this link to a friend to challenge them</DialogContentText>
+
+        <FormControl variant="outlined" fullWidth margin="dense">
+          <InputLabel htmlFor="challengeLinkTextField">Challenge link</InputLabel>
+
+          <OutlinedInput
+            id="challengeLinkTextField"
+            inputRef={inputRef}
+            autoFocus
+            type="url"
+            readOnly
+            label="Challenge link"
+            value={link}
+            onClick={selectInputText}
+            endAdornment={
+              <InputAdornment
+                position="end"
+                onMouseEnter={onMouseEnterAdornment}
+                onMouseLeave={onMouseLeaveAdornment}
+              >
+                <Tooltip open={tooltipOpen} title={tooltipTitle}>
+                  <IconButton size="small" edge="end" onClick={onCopyToClipboardClick}>
+                    <AssignmentTurnedInOutlined />
+                  </IconButton>
+                </Tooltip>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
       </DialogContent>
 
       <DialogActions>
