631b5444857dedc3c4a02daa9a7e7376c92dddd5
aob3318 <aob3318@ic.ac.uk>
2020-12-23 18:31:14 +0200

    fix: Resolve facebook share incorect number of points and add thumbnail image

--- src/screens/game/ShareMenu.tsx
+++ src/screens/game/ShareMenu.tsx
@@ -54,7 +54,7 @@ const ShareMenu: React.FC<ShareMenuProps> = ({ playerScore }: ShareMenuProps) =>
           <FacebookShareButton
             className={classes.shareButton}
             url="http://cb3618.pages.doc.ic.ac.uk/spot-the-lesion"
-            title={`I got ${playerScore} points in Spot-the-Lesion! Can you beat my score?`}
+            title={`I got ${playerScore.total} points in Spot-the-Lesion! Can you beat my score?`}
           >
             <FacebookIcon size="50px" round />
           </FacebookShareButton>
