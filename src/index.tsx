7b43338596a729d183faca5a5e257b05ea065b8a
cb3618 <cb3618@ic.ac.uk>
2020-12-26 22:22:58 +0200

    chore: Update npm packages
    Remove @material-ui/styles, firebase-admin

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { getGlobalVariables, setupFirebase } from "./firebase/firebaseApp";
-import "fontsource-roboto/300.css";
-import "fontsource-roboto/400.css";
-import "fontsource-roboto/500.css";
-import "fontsource-roboto/700.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 /* Set up firebase before rendering */
 setupFirebase();
--- src/screens/leaderboard/scoreTabel/BasicTable.tsx
+++ src/screens/leaderboard/scoreTabel/BasicTable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -8,7 +8,6 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { FaMedal } from "react-icons/fa";
-import { createStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
