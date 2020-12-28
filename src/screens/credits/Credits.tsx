2fa9c95264046113fb0dcda774e6ff9a8883214f
Calin Biberea <calinbiberea@gmail.com>
2020-12-28 17:52:30 +0200

    feat(App): Create tabs for credits page to show licenses

--- src/screens/credits/Credits.tsx
+++ src/screens/credits/Credits.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Typography } from "@material-ui/core";
+import { AppBar, Card, Tab, Tabs, Typography } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { NavigationAppBar } from "../../components";
 
@@ -34,46 +34,89 @@ const useStyles = makeStyles((theme) =>
         fontSize: "2rem",
       },
     },
+    appBar: {
+      alignItems: "center",
+      backgroundColor: "#004445",
+    },
+    tabIndicator: {
+      backgroundColor: "#C4DFE6",
+    },
+    tab: {
+      fontSize: "1rem",
+    },
   })
 );
 
 const Credits: React.FC = () => {
   const classes = useStyles();
 
+  const [currentTableIndex, setCurrentTableIndex] = React.useState(0);
+
+  const getTableContent = (tableIndex) => {
+    if (tableIndex === 0) {
+      return (
+        <div className={classes.container}>
+          <Card className={classes.card}>
+            <Typography className={classes.text}>
+              This demo is based on our{" "}
+              <a href="https://arxiv.org/abs/1906.02283" target="blank">
+                MICCAI 2019 paper
+              </a>
+            </Typography>
+
+            <Typography className={classes.text}>
+              CT Scan AI developed by Martin Zlocha, Qi Dou and Ben Glocker.
+            </Typography>
+
+            <Typography className={classes.text}>
+              This site was made with React for the 3rd year Software Engineering Group Project by:
+              Andrei-Matei Roman, Andrei-Ovidiu Badea, Calin-Andrei Alexandru, Calin Biberea,
+              Cosmin-Ionut Baies, Tiberiu-Andrei Georgescu
+            </Typography>
+
+            <Typography className={classes.text}>
+              (c) 2019 Data obtained from the{" "}
+              <a
+                href="https://www.nih.gov/news-events/news-releases/nih-clinical-center-releases-dataset-32000-ct-images"
+                target="blank"
+              >
+                NIH Clinical Center
+              </a>
+            </Typography>
+          </Card>
+        </div>
+      );
+    }
+    return <div>Revolution</div>;
+  };
+
   return (
     <>
       <NavigationAppBar showBack />
+      <AppBar className={classes.appBar} position="sticky">
+        <Tabs
+          value={currentTableIndex}
+          onChange={(_, newTableIndex) => setCurrentTableIndex(newTableIndex)}
+          aria-label="Leaderboards"
+          classes={{ indicator: classes.tabIndicator }}
+        >
+          <Tab
+            className={classes.tab}
+            label="About The Game"
+            id="game-info"
+            aria-controls="about-view-0"
+          />
 
-      <div className={classes.container}>
-        <Card className={classes.card}>
-          <Typography className={classes.text}>
-            This demo is based on our{" "}
-            <a href="https://arxiv.org/abs/1906.02283" target="blank">
-              MICCAI 2019 paper
-            </a>
-          </Typography>
+          <Tab
+            className={classes.tab}
+            label="Libraries & Images"
+            id="game-credits"
+            aria-controls="about-view-1"
+          />
+        </Tabs>
+      </AppBar>
 
-          <Typography className={classes.text}>
-            CT Scan AI developed by Martin Zlocha, Qi Dou and Ben Glocker.
-          </Typography>
-
-          <Typography className={classes.text}>
-            This site was made with React for the 3rd year Software Engineering Group Project by:
-            Andrei-Matei Roman, Andrei-Ovidiu Badea, Calin-Andrei Alexandru, Calin Biberea,
-            Cosmin-Ionut Baies, Tiberiu-Andrei Georgescu
-          </Typography>
-
-          <Typography className={classes.text}>
-            (c) 2019 Data obtained from the{" "}
-            <a
-              href="https://www.nih.gov/news-events/news-releases/nih-clinical-center-releases-dataset-32000-ct-images"
-              target="blank"
-            >
-              NIH Clinical Center
-            </a>
-          </Typography>
-        </Card>
-      </div>
+      {getTableContent(currentTableIndex)}
     </>
   );
 };
