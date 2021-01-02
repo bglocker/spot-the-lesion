75fe10dba2e345cc6b51675ef294ba2b1ea4eed9
Calin Biberea <calinbiberea@gmail.com>
2021-01-02 17:24:28 +0200

    feat(Credits): Modify syntax for tests

--- src/tests/utils/numberUtils.test.tsx
+++ src/tests/utils/numberUtils.test.tsx
@@ -1,24 +1,28 @@
 import { modulo, randomAround } from "../../utils/numberUtils";
 
-test("modulo works for positive dividend and divider", () => {
-  expect(modulo(1, 2)).toBe(1);
-  expect(modulo(6, 3)).toBe(0);
-  expect(modulo(5, 3)).toBe(2);
+describe("Modulo", () => {
+  it("calculates correctly for positive dividend and divider", () => {
+    expect(modulo(1, 2)).toBe(1);
+    expect(modulo(6, 3)).toBe(0);
+    expect(modulo(5, 3)).toBe(2);
+  });
+
+  it("calculates correctly for positive dividend and negative divider", () => {
+    expect(modulo(1, -2)).toBe(-1);
+    expect(modulo(6, -3)).toBe(-0);
+    expect(modulo(5, -3)).toBe(-1);
+  });
+
+  it("calculates correctly for negative dividend and positive divider", () => {
+    expect(modulo(-1, 2)).toBe(1);
+    expect(modulo(-6, 3)).toBe(0);
+    expect(modulo(-5, 3)).toBe(1);
+  });
 });
 
-test("modulo works for positive dividend and negative divider", () => {
-  expect(modulo(1, -2)).toBe(-1);
-  expect(modulo(6, -3)).toBe(-0);
-  expect(modulo(5, -3)).toBe(-1);
-});
-
-test("modulo works for negative dividend and positive divider", () => {
-  expect(modulo(-1, 2)).toBe(1);
-  expect(modulo(-6, 3)).toBe(0);
-  expect(modulo(-5, 3)).toBe(1);
-});
-
-test("random around gives a number in a range given a start point", () => {
-  expect(randomAround(1, 2)).toBeGreaterThanOrEqual(-1);
-  expect(randomAround(1, 2)).toBeLessThanOrEqual(3);
+describe("Random Around", () => {
+  it("gives a number in a range given a start point", () => {
+    expect(randomAround(1, 2)).toBeGreaterThanOrEqual(-1);
+    expect(randomAround(1, 2)).toBeLessThanOrEqual(3);
+  });
 });
