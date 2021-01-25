5feeec248114840643cb1d91f06dee73b586453f
cb3618 <cb3618@ic.ac.uk>
2021-01-25 19:35:14 +0200

    fix: Remove temporary console warns on env variables
    Remove env context from deploy step

--- src/firebase/firebaseApp.ts
+++ src/firebase/firebaseApp.ts
@@ -35,9 +35,6 @@ const firebaseConfig = {
  * @return True if successful, false otherwise
  */
 const initializeFirebase = async (): Promise<boolean> => {
-  console.warn(process.env.REACT_APP_FIREBASE_API_KEY);
-  console.warn(process.env.REACT_APP_SERVER_KEY);
-
   if (process.env.REACT_APP_FIREBASE_API_KEY === undefined) {
     console.error("Firebase API key not set.");
 
